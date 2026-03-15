0b1a4a9c2127d5e3ead0c2bcf8c7738db49c9ff4
Johan Tibell <johan.tibell@gmail.com>
2026-03-15 07:58:49 +0100

    add ruff lsp to nvim

--- nvim/lua/plugins/ty.lua
+++ nvim/lua/plugins/ty.lua
@@ -4,7 +4,36 @@ return {
     opts = {
       servers = {
         ty = {},
+        ruff = {
+          init_options = {
+            settings = {
+              logLevel = "error",
+            },
+          },
+          keys = {
+            {
+              "<leader>co",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
+            },
+          },
+        },
+      },
+      setup = {
+        ruff = function()
+          Snacks.util.lsp.on({ name = "ruff" }, function(_, client)
+            -- Keep hover/docs with ty and let ruff handle lint actions only.
+            client.server_capabilities.hoverProvider = false
+          end)
+        end,
       },
     },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.python = { "ruff_organize_imports", "ruff_fix", "ruff_format" }
+    end,
   },
 }
