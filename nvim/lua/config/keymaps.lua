51e6baab6ccee2eeeb5905ad5dc2db9ffb4862c1
Johan Tibell <johan.tibell@gmail.com>
2026-04-18 06:41:58 +0200

    nvim: don't override <leader><space>
    There's an existing <leader>fF that does the same thing.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-vim.keymap.del("n", "<leader><space>")
-vim.keymap.set("n", "<leader><space>", LazyVim.pick("files", { root = false, hidden = true }), {
-  desc = "Find Files (cwd)",
-})
